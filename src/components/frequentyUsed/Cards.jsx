f74096f31d8213d1a220a37dcda66035f0906525
alwaysAnsh <anshj9y@gamil.com>
2024-07-09 00:14:56 +0530

    client requirement changes

--- src/components/frequentyUsed/Cards.jsx
+++ src/components/frequentyUsed/Cards.jsx
@@ -35,27 +35,8 @@ import {
           </Typography>
         </CardBody>
         <CardFooter className="">
-          {/* <div className="flex items-center -space-x-3">
-            <Tooltip content="Natali Craig">
-              <Avatar
-                size="sm"
-                variant="circular"
-                alt="natali craig"
-                src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1061&q=80"
-                className="border-2 border-white hover:z-10"
-              />
-            </Tooltip>
-            <Tooltip content="Tania Andrew">
-              <Avatar
-                size="sm"
-                variant="circular"
-                alt="tania andrew"
-                src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-                className="border-2 border-white hover:z-10"
-              />
-            </Tooltip>
-          </div> */}
-          {/* <Typography className="font-normal text-darkblue">Explore</Typography> */}
+          
+          
         </CardFooter>
       </Card>
     );
